e6915038a87c5161c7bc1bb7e0eb2ffd2d68a8c3
lovelyjuice <lovelyjuice@github.com>
2025-01-08 17:46:48 +0800

    Initial commit

---
+++ main.js
@@ -0,0 +1,314 @@
+const {
+  Plugin,
+  Modal,
+  Notice,
+  PluginSettingTab,
+  Setting,
+  FileSystemAdapter
+} = require("obsidian");
+const https = require("https");
+const path = require("path");
+
+class ImageDownloaderSettingTab extends PluginSettingTab {
+  constructor(app, plugin) {
+    super(app, plugin);
+    this.plugin = plugin;
+  }
+
+  display() {
+    const { containerEl } = this;
+
+    containerEl.empty();
+    containerEl.createEl("h2", { text: "Image Downloader Settings" });
+
+    new Setting(containerEl)
+      .setName("Image Save Directory")
+      .setDesc(
+        "Image save directory is the same as Obsidian's attachment default directory, no need to extra configure"
+      );
+    // .addText((text) =>
+    //   text
+    //     .setPlaceholder(DefaultConfig.assetsDir)
+    //     .setValue(this.plugin.settings.assetsDir)
+    //     .onChange(async (value) => {
+    //       this.plugin.settings.assetsDir = value;
+    //       await this.plugin.saveSettings();
+    //     })
+    // );
+  }
+}
+
+class DefaultConfig {
+  static assetsDir = "assets";
+  static obsmediadir = "";
+}
+
+class RefererModal extends Modal {
+  defaultReferer = "";
+  constructor(app, callback) {
+    super(app);
+    this.callback = callback;
+  }
+
+  onOpen() {
+    const { contentEl } = this;
+    contentEl.empty(); // 清空内容
+    const label = contentEl.createEl("label", {
+      text: "Please input Referer (URL):",
+    });
+    contentEl.createEl("br");
+    const input = contentEl.createEl("input", {
+      type: "text",
+      id: "referer-input",
+      placeholder: "https://example.com/xxx/",
+    });
+    input.style.margin = "10px";
+    input.style.marginLeft = "5px";
+    input.style.width = "80%";
+    if (this.defaultReferer) {
+      input.value = this.defaultReferer; // 设置默认Referer
+    }
+
+    const confirmButton = contentEl.createEl("button", { text: "OK" });
+    confirmButton.addEventListener("click", () => {
+      const referer = input.value;
+      if (!referer) {
+        alert("Referer cannot be empty!");
+        return; // If user did not input Referer, exit function
+      }
+      this.callback(referer);
+      this.close();
+    });
+
+    input.addEventListener("keydown", (event) => {
+      if (event.key === "Enter") {
+        confirmButton.click(); // 按下 Enter 键时触发确认按钮。这里有bug，会导致编辑器也收到Enter按键事件从而导致文档开头会多一个回车
+      }
+    });
+  }
+
+  onClose() {
+    const { contentEl } = this;
+    contentEl.empty();
+  }
+}
+
+module.exports = class ImageDownloaderPlugin extends Plugin {
+  async onload() {
+    await this.loadSettings(); // Ensure settings are loaded
+
+    this.addCommand({
+      id: "download-images",
+      name: "Download images with referer",
+      callback: () => this.downloadImages(),
+    });
+
+    this.addSettingTab(new ImageDownloaderSettingTab(this.app, this));
+  }
+
+  async loadSettings() {
+    const data = await this.loadData();
+    this.settings = Object.assign({ assetsDir: DefaultConfig.assetsDir }, data); // 设置默认值
+  }
+
+  async saveSettings() {
+    await this.saveData(this.settings);
+  }
+
+  async downloadWithReferer(referer) {
+    let downloadDir = "";
+    if (DefaultConfig.obsmediadir.startsWith(".")) {
+      //当前文件所在文件夹(./) + 当前文件所在文件夹下的子文件夹(./assets)
+      downloadDir = path.join(
+        path.dirname(this.app.workspace.getActiveFile().path),
+        DefaultConfig.obsmediadir
+      );
+    } else {
+      // 仓库根目录(/) + 指定的附件文件夹(attachment)
+      downloadDir = DefaultConfig.obsmediadir;
+    }
+    const imageUrls = this.extractImageUrls(this.content);
+    const downloadedPathsMap = new Map(); // 用于存储下载的文件路径
+    // 检查并创建目录
+    try {
+      await FileSystemAdapter.mkdir(path.join(this.app.vault.adapter.basePath, downloadDir), {
+        recursive: true,
+      });
+      console.log(
+        "Directory created: ",
+        path.join(this.app.vault.adapter.basePath, downloadDir)
+      );
+    } catch (err) {
+      console.error("Directory creation failed:", err);
+      new Notice("Directory creation failed, please check permissions!");
+      return;
+    }
+
+    for (const url of imageUrls) {
+      try {
+        const fileName = await this.downloadImage(url, downloadDir, referer);
+        downloadedPathsMap.set(
+          url,
+          path.join(DefaultConfig.obsmediadir, fileName).replaceAll("\\", "/")
+        ); // 收集下载的文件路径, 反斜杠替换成斜杠是因为obsidian在markdown中不支持反斜杠
+      } catch (error) {
+        console.error(
+          `Download failed: ${url}, error message: ${error.message}`
+        );
+      }
+    }
+
+    const updatedContent = this.replaceImageUrls(
+      this.content,
+      downloadedPathsMap
+    );
+    await this.app.vault.modify(this.activeFile, updatedContent);
+    new Notice("Images download completed!");
+  }
+
+  async downloadImages() {
+    DefaultConfig.obsmediadir = this.app.vault.getConfig(
+      "attachmentFolderPath"
+    );
+    console.debug("obsmediadir: ", DefaultConfig.obsmediadir);
+
+    this.activeFile = this.app.workspace.getActiveFile();
+    if (!this.activeFile) {
+      console.warn("You haven't open a document!");
+      return;
+    }
+    let disableModal = false;
+    let defaultReferer = "";
+    // 优先从文档属性中获取Referer
+    app.fileManager.processFrontMatter(this.activeFile, (frontmatter) => {
+      for (const key in frontmatter) {
+        if (
+          typeof frontmatter[key] === "string" &&
+          frontmatter[key].startsWith("http")
+        ) {
+          console.log(
+            `Found Referer from properties of document. ${key}: ${frontmatter[key]}`
+          );
+          defaultReferer = frontmatter[key];
+          disableModal = true;
+        }
+      }
+    });
+
+    const activeAbsolutePath = `${this.app.vault.adapter.basePath}/${this.activeFile.path}`;
+    console.debug("Current active file path: ", activeAbsolutePath);
+
+    this.content = await this.app.vault.read(this.activeFile);
+
+    if (defaultReferer === "") {
+      // 如果文档属性中没有Referer，查找文档前200个字符中的第一个URL
+      const first200Chars = this.content.slice(0, 200);
+      const urlMatch = first200Chars.match(/https?:\/\/[^\s]+/);
+      if (urlMatch) {
+        defaultReferer = urlMatch[0];
+        console.debug("Found Referer from file content.", urlMatch[0]);
+      }
+    }
+
+    if (!disableModal) {
+      const modal = new RefererModal(app, async (referer) => {
+        await this.downloadWithReferer(referer);
+      });
+      // 在弹框中填充默认Referer
+      modal.defaultReferer = defaultReferer;
+      modal.open();
+    } else this.downloadWithReferer(defaultReferer);
+  }
+
+  replaceImageUrls(content, downloadedPaths) {
+    return content.replace(/!\[(.*?)\]\((http.*?)\)/g, (match, ...p) => {
+      console.debug("match: ", match);
+      console.debug("p: ", p);
+      const downloadedPath = downloadedPaths.get(p[1]); // 从 Map 中获取下载路径
+      if (!downloadedPath) return match; // 如果没有下载该URL，则返回原始内容
+      return `![${p[0]}](${downloadedPath})`; // 使用下载的路径
+    });
+  }
+
+  extractImageUrls(content) {
+    const regex = /!\[.*?\]\((http.*?)\)/g;
+    const urls = [];
+    let match;
+    while ((match = regex.exec(content)) !== null) {
+      urls.push(match[1]);
+    }
+    return urls;
+  }
+
+  async downloadImage(url, path, referer) {
+    console.debug("Start downloading image:", url);
+    let options = {};
+    if (referer) {
+      options = {
+        headers: {
+          Referer: encodeURI(referer),
+        },
+      };
+    }
+
+    return new Promise((resolve, reject) => {
+      https
+        .get(url, options, async (response) => {
+          let data = [];
+          const contentType = response.headers["content-type"];
+          let extension = "";
+
+          // 根据 Content-Type 确定文件扩展名
+          const typeMap = {
+            jpeg: ".jpg",
+            jpg: ".jpg",
+            png: ".png",
+            gif: ".gif",
+            webp: ".webp",
+            "svg+xml": ".svg",
+            tiff: ".tiff",
+            bmp: ".bmp",
+            ico: ".ico",
+            avif: ".avif",
+            heic: ".heic",
+            heif: ".heif",
+          };
+          const type = contentType.split("/")[1];
+          if (typeMap[type]) {
+            extension = typeMap[type];
+          } else {
+            console.error(
+              "Unsupported file type:",
+              contentType,
+              "for URL:",
+              url
+            );
+            reject(new Error("Unsupported file type: " + contentType));
+            return;
+          }
+
+          response.on("data", (chunk) => {
+            data.push(chunk);
+          });
+
+          response.on("end", async () => {
+            const buffer = Buffer.concat(data);
+            console.debug(`Image file size: ${buffer.length} bytes`);
+
+            // 生成随机文件名
+            const timestamp = Date.now();
+            const randomNum = Math.floor(100 + Math.random() * 900); // 生成3位随机数
+            const fileName = `${timestamp}-${randomNum}${extension}`;
+            const filePath = `${path}/${fileName}`;
+
+            await this.app.vault.createBinary(filePath, buffer);
+            resolve(fileName);
+          });
+        })
+        .on("error", (err) => {
+          console.error(`Error downloading image[${url}]:`, err.message);
+          reject(err);
+        });
+    });
+  }
+};
